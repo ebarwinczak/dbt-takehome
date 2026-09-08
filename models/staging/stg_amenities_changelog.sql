68cbdbc4708f21ba11a9041b14dd0a5e955617bd
Elizabeth Barwinczak <elliebarwinczak@Elizabeths-MacBook-Air.local>
2026-09-08 14:11:29 -0400

    remove unnecessary cleaning of listing_id in stg_amenities_changelog

--- models/staging/stg_amenities_changelog.sql
+++ models/staging/stg_amenities_changelog.sql
@@ -5,9 +5,9 @@
 }}
 
 SELECT
-    CAST({{ clean_string('CAST(LISTING_ID AS VARCHAR)') }} AS INTEGER) AS listing_id, -- in case there are blanks in the CSV
+    LISTING_ID AS listing_id, -- in case there are blanks in the CSV
     CAST(CHANGE_AT AS TIMESTAMP) AS change_at,
     FROM_JSON(LOWER({{ clean_string('AMENITIES') }}), '["VARCHAR"]') AS amenities_list
 
 FROM {{ ref('AMENITIES_CHANGELOG')}}
-WHERE {{ clean_string('CAST(LISTING_ID AS VARCHAR)') }} IS NOT NULL 
+WHERE LISTING_ID IS NOT NULL 
